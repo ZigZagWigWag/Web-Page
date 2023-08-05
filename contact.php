3fd2678b32cab2bad8ae8a5bd4dd53d8f1fa12c7
ZigZagWigWag <tricothekids8@gmail.com>
2023-08-05 14:27:31 +0800

    contact and js
    added contach and unfinished js

--- contact.php
+++ contact.php
@@ -33,18 +33,46 @@
         <h3>Contacts</h3>
     </div>
 
-    <!--form-->
+    <!--contact-->
   <div class="contacts">
-    <div class="penyelia"></div>
-    <div class="ketua"></div>
+    <div class="ketua">
+        <img src="img/ketua.jpeg" alt="">
+        <h1>Ketua Asrama</h1>
+        <button onclick="myFunction1()" id="myBtn">Read more</button>
+        <span id="more1">asasas</span>
+    </div>
+    <div class="penyelia">
+        <img src="img/penyelia.jpg" alt="">
+        <h1>Penyelia Asrama</h1>
+        <button onclick="myFunction2()" id="myBtn">Read more</button>
+        <span id="more2">asasas</span>
+    </div>  
   </div>
     <!--footer-->
 
     <div class="footer">
         <p>Copyright &copy; 2023 <span>Kolej Vokasional Sepang</span></p>
     </div>
-    <script src="script.js"></script>
-
+    
     </div>
+    <script>
+        function myFunction1() {
+    var x = document.getElementById("more1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("more2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+    </script>
 </body>
 </html> 
