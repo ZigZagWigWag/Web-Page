00a96bd10d568c5f79b3454b7a69f332a0c1b2cb
Fiq <111990744+Afiq-penyu@users.noreply.github.com>
2023-08-30 01:03:37 +0800

    senarai aduan admin

---
+++ table_aduan.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senarai Aduan</title>
+</head>
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="table_aduan.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <title>Aduan Asrma KVS</title>
+</head>
+<body>
+
+    <div class="container">
+
+    <!--navbar-->
+
+    <div class="navbar">
+        <div class="navlogo">
+            <div class="logo">
+                <a href="index.php"><img src="img/logokv.png" alt=""></a>
+            </div>
+        </div>
+        <ul class="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contact.php">Contacts</a></li>
+            <li><a href="admin.php">Admin</a></li>
+        </ul>
+    </div>
+
+   <!--text-->
+
+    <div class="text">
+        <h3>Senarai Aduan</h3>
+    </div>
+
+    <!-- table -->
+    <table cellpadding="5" cellspacing="0">
+        <tr>
+            <th>NAMA</th>
+            <th>BLOK</th>
+            <th>DORM</th>
+            <th>ADUAN</th>
+            <th>TARIKH</th>
+            <th>TINDAKAN</th>
+        </tr>
+
+        <?php 
+                $result=mysqli_query($conn, "SELECT * FROM ");
+                while($row=mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo "<td>".$row['Nama']."</td>";
+                    echo "<td>".$row['Blok']."</td>";
+                    echo "<td>".$row['Dorm']."</td>";
+                    echo "<td>".$row['Aduan']."</td>";
+                    echo "<td>".$row['Tarikh']."</td>";
+                    echo"<td><a href= \"delete.php?Nama=$row[Nama]\" onClick=\return confrim(Adakah anda pasti?)\">Delete</a></td>";
+                }
+        ?>
+    </table>
+    <!--footer-->
+
+    <div class="footer">
+        <p>Copyright &copy; 2023 <span>Kolej Vokasional Sepang</span></p>
+    </div>
+
+    </div>
+</body>
+</html> 
+    
+</body>
+</html>
