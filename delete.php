b30d196a485f53844526991530b9752a2239358c
ZigZagWigWag <tricothekids8@gmail.com>
2023-09-01 02:55:19 +0800

    final 3

--- delete.php
+++ delete.php
@@ -6,6 +6,6 @@
 
     $result = mysqli_query($conn, "DELETE FROM aduan WHERE id='$id'");
     echo "<script>alert('Memadam Data Berjaya');"
-	. "window.location='table_aduan.php'</script>";
+	. "window.location='admin.php'</script>";
 
 ?>
